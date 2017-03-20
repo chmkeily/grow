c7992996f2cf384ec1ab55acb052f79595e9f152
hemingchen <chmkeily@gmail.com>
2017-03-20 22:02:48 +0800

    +randi64.h

---
+++ src/randi46.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: randi46.h
+    > Author: hemingchen
+    > Mail: chmkeily@gmail.com 
+    > Created Time: 2017-03-20 22:02:23
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
