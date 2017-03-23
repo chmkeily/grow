f0cb602c209992cbd6e503bac62e60a4f8968d21
hemingchen <chmkeily@gmail.com>
2017-03-23 22:10:44 +0800

    + headfiles

--- src/sys_hook.cpp
+++ src/sys_hook.cpp
@@ -1,11 +1,12 @@
 #define __USE_GNU
 #include <dlfcn.h>
 #include <stdio.h>
+#include <unistd.h>
 
 typedef ssize_t (*read_pfn_t)(int fildes, void *buf, size_t nbyte);
 static read_pfn_t g_sys_read_func = (read_pfn_t)dlsym(RTLD_NEXT, "read");
 
 ssize_t read(int fildes, void *buf, size_t nbyte)
 {
-    return g_sys_read_func(fd, buf, nbyte);
+    return g_sys_read_func(fildes, buf, nbyte);
 }
