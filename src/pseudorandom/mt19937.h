a99d852c378c3deee0cb3165cce15f49110b9c57
HemingCHEN <chmkeily@gmail.com>
2017-04-11 17:57:18 +0800

    Update mt19937.h

--- src/pseudorandom/mt19937.h
+++ src/pseudorandom/mt19937.h
@@ -5,5 +5,27 @@
     > Mail: chmkeily@gmail.com 
     > Created Time: 2017-04-11 16:58:33
  ************************************************************************/
- 
- 
+
+#include <stblib.h
+#include <time.h>
+
+typedef long long int int64_t;
+
+namespace xgrow
+{
+
+static int g_index;
+static int64_t g_MT[614];  
+     
+void srand(int64_t seed)
+{
+    
+}
+
+int64_t rand()
+{
+    
+    return 0;
+}
+     
+}
