5a3e94d16f4b8b927c1cee97b18d59ff3c7d2d3e
HemingCHEN <chmkeily@gmail.com>
2017-06-18 13:06:03 +0800

    有序对象池
    有序对象池-包含有序列表，并且有O(logN)级别键值随机访问和更新效率

---
+++ src/template/SortedObjectPool.h
@@ -0,0 +1,16 @@
+#ifndef _SORTED_OBJECT_POOL_H_
+#define _SORTED_OBJECT_POOL_H_
+
+#include <iosteam>
+using namespace std;
+
+template <type T>
+class CSortedObjectPool
+{
+public:
+
+priavte:
+
+};
+
+#endif
