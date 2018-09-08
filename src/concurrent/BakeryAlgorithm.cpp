7dd5efa5bcc5acd0f631f2b964667889e092b6d8
HemingCHEN <chmkeily@gmail.com>
2018-09-08 20:43:01 +0800

    Update BakeryAlgorithm.cpp

--- src/concurrent/BakeryAlgorithm.cpp
+++ src/concurrent/BakeryAlgorithm.cpp
@@ -1,1 +1,38 @@
 #include <iostream>
+using namespace std;
+
+#define N 10
+bool g_Choosing[N];
+int g_Number[N];
+
+int init()
+{
+  for (int i = 0; i < N; i ++)
+  {
+    g_Choosing[i] = 0;
+    g_Number[i] = 0;
+  }
+
+  return 0;
+}
+
+bool checkIfTurn()
+{
+  //算法实现
+  return false;
+}
+
+int critical_proc()
+{
+  return 0;
+}
+
+int main()
+{
+  //init global env
+  init();
+
+  //create threads
+  
+  return 0;
+}
