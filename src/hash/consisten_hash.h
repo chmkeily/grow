b6615e707616ef9478d57266eff40674e9db1783
HemingCHEN <chmkeily@gmail.com>
2018-12-29 15:41:35 +0800

    Update consisten_hash.h

--- src/hash/consisten_hash.h
+++ src/hash/consisten_hash.h
@@ -1,7 +1,13 @@
+/**
+* @author   hemingchen
+* @date     Dec 2018
+*/
+
 #include <stdlib.h>
 
 /**
 * @brief 一致性哈希 (本质：通过在Hash值和Node之间增加一层映射，以保证Key到Node的最终映射保持不变。)
+* @keywords 一致性哈希，均衡分布，虚拟节点
 */
 class CConsistenHash
 {
