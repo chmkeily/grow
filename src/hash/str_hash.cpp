1430aceee88a35eb36bda166a9209834c31bf464
HemingCHEN <chmkeily@gmail.com>
2019-02-19 16:30:35 +0800

    增加murmur_hash2哈希算法

--- src/hash/str_hash.cpp
+++ src/hash/str_hash.cpp
@@ -3,7 +3,7 @@
 *   Hash = Hash * 33 + c
 */
 
-unsigned int djb_hash(char *key)
+unsigned int djb_hash(const char *key)
 {
     unsigned int hash = 5381;
     while (key)
@@ -12,4 +12,14 @@ unsigned int djb_hash(char *key)
     }
     
     return hash.
+}
+
+/******************
+* Murmurhash2哈希算法 - Redis采用此算法作为其字典底层哈希表的哈希算法
+*   ref: https://github.com/aappleby/smhasher/blob/master/src/MurmurHash2.cpp
+*/
+
+unsigned int murmur_hash2(const char *key, int len, unsigned int seed)
+{
+    // 请参见Murmurhash2库的实现
 }
